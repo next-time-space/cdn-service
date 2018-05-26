0fb32f398f6ca6c063647765a5c15c791ba7bf9c
pasu <pasu_pathi@outlook.com>
2018-05-26 10:45:26 -0500

    Cov

--- src/test/java/com/nexttimespace/cdnservice/utility/UtilityFunctionsTest.java
+++ src/test/java/com/nexttimespace/cdnservice/utility/UtilityFunctionsTest.java
@@ -8,6 +8,8 @@ public class UtilityFunctionsTest {
 
 	@Test
 	public void findMediaTypeTest() {
+		UtilityFunctions uf = new UtilityFunctions();
+		
 		Assertions.assertEquals(MediaType.TEXT_PLAIN, UtilityFunctions.findMediaType("hello"));
 		Assertions.assertEquals("text/css",UtilityFunctions.findMediaType("hello.css").getType() + "/" + UtilityFunctions.findMediaType("hello.css").getSubtype());
 		Assertions.assertEquals("text/javascript", UtilityFunctions.findMediaType("hello.js").getType() + "/" + UtilityFunctions.findMediaType("hello.js").getSubtype());
